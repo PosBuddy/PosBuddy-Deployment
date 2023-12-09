b4ba6064ff773266ce5a5866db3fa0c1d8e83336
jkarthaus <Joern.karthaus@gmail.com>
2023-12-09 09:16:36 +0100

    add example items.sql

---
+++ exampleData/items.sql
@@ -0,0 +1,6 @@
+INSERT INTO public.items (id, artikel, unit, minage, dispensingstation, price)
+VALUES ('coke', 'Coka Cola', 'Glas', 0, 'bar1', 2.2);
+INSERT INTO public.items (id, artikel, unit, minage, dispensingstation, price)
+VALUES ('fanta', 'Fanta Orange', 'Glas', 0, 'bar1', 2.2);
+INSERT INTO public.items (id, artikel, unit, minage, dispensingstation, price)
+VALUES ('wurst', 'Bratwurst', 'Stück', 0, 'grill', 2.5);
