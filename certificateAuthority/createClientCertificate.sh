9fb96c023a948e63da6a0bdc5da7d2ea6b3708dc
jkarthaus <Joern.karthaus@gmail.com>
2024-01-16 21:26:52 +0100

    add test checkout certificate

--- certificateAuthority/createClientCertificate.sh
+++ certificateAuthority/createClientCertificate.sh
@@ -1,8 +1,18 @@
 
 clear
 
-echo "you need 0..n serve station certificate           "
+echo "you need 0..n serve station certificate             "
 echo "an min. 1 checkout station certificate              "
 
 # Step 1 create a certificate request
-openssl req -newkey rsa:4096 -nodes -keyout client.key -out client.csr
+# Set here the Common Name CN=serve / CN=checkout
+#openssl req -newkey rsa:4096 -nodes -keyout client.key -out client.csr
+
+# Step 2 Sign the client CSR with the CA
+#openssl x509 -req -CA cacert.pem -CAkey cacert.key -in client.csr -out client.pem -days 365 -CAcreateserial
+
+# Step 3 optinal verify and inspect the certificate
+#openssl x509 -in client.pem -text
+
+# Step 4 Bundle the certificate and private key in PKCS#12 format
+#openssl pkcs12 -export -out client.p12 -name "client" -inkey client.key -in client.pem
--- exampleData/examplePosBuddyData.sql
+++ exampleData/examplePosBuddyData.sql
@@ -4,3 +4,6 @@ VALUES ('a89cee33-7e12-42c6-bffe-1b3a3c5fca64', 'Wallace', 'Chedar', '1975-03-08
 
 INSERT INTO public.identity (posbuddyid, surname, lastname, birthday, atribute1, atribute2, atribute3, startallocation, endallocation, balance)
 VALUES ('6be777b9-4cfe-4c3b-a8c5-5f6874efe84f', 'Gromit', 'Dog', '2006-06-09', '4 legs', null, null, '2023-05-20', null, 45.33);
+
+INSERT INTO public.revenues (id, posbuddyid, itemtext, amount, paymentaction, value, timeofaction, itemid)
+VALUES (9, '6be777b9-4cfe-4c3b-a8c5-5f6874efe84f', 'Bratwurst', 10, 'R', 30, '2024-01-16 21:20:32.820813', null);
