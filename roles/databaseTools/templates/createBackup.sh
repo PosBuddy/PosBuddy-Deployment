79e41899030b74c60cf326e7d6ec083d5e049e2b
jkarthaus <Joern.karthaus@gmail.com>
2024-05-01 13:56:09 +0200

    fix backup script

--- roles/databaseTools/templates/createBackup.sh
+++ roles/databaseTools/templates/createBackup.sh
@@ -1,10 +1,11 @@
 #! /bin/bash
+
 TIMESTAMP=$(date +%Y-%m-%d_%H-%M-%S)
 
 echo "Backup posBuddy Database"
-docker exec -u postgres posBuddy-database /bin/bash -c "/usr/bin/pg_dump -U $POSTGRES_USER posbuddy" | gzip -9 >/tmp/postgres-backup.sql.gz
+docker exec -u postgres posBuddy-database /bin/bash -c "/usr/bin/pg_dump -U $POSTGRES_USER posbuddy | gzip -9 > /tmp/postgres-backup.sql.gz"
 
 echo "copy backup from container"
-docker cp posBuddy-database:/tmp/postgres-backup.sql.gz /opt/posBuddy/backup_posBuddy_$TIMESTAMP.tgz
+docker cp posBuddy-database:/tmp/postgres-backup.sql.gz /opt/posBuddy/backup_posBuddy_$TIMESTAMP.sql.gz
 
 echo "Backup created"
